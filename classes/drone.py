505b51d776a10669d34af2afee7376abe2663966
schoningerJim <schoninger@enroute.co.jp>
2018-12-18 17:19:28 +0900

    in the drone class, add some functions to move the drone and send the position to the web API

--- classes/drone.py
+++ classes/drone.py
@@ -4,6 +4,7 @@ from socketIO_client_nexus import SocketIO, LoggingNamespace, ConnectionError
 from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal
 import json
 import time
+import math
 from classes.interval import Interval
 
 class Drone(object):
@@ -13,9 +14,69 @@ class Drone(object):
         self.interval = None
         self.vehicle = None
         self.socketIO = None
+        # test square path drone
+        self.dronePosLisIdx = 0
+        self.dronePosList = []
+        self.dronePosList.append({
+            "type": "marker",
+            "coordinates": [
+                139.58855534,
+                35.82004085
+
+             ],
+            "properties": {
+                "id": str(time.time())
+            }
+        })
+        self.dronePosList.append({
+            "type": "marker",
+            "coordinates": [
+                139.588850,
+                35.820344
+
+             ],
+            "properties": {
+                "id": str(time.time())
+            }
+        })
+        self.dronePosList.append({
+            "type": "marker",
+            "coordinates": [
+                139.589331,
+                35.820088
+
+             ],
+            "properties": {
+                "id": str(time.time())
+            }
+        })
+        self.dronePosList.append({
+            "type": "marker",
+            "coordinates": [
+                139.588671,
+                35.819371
+
+             ],
+            "properties": {
+                "id": str(time.time())
+            }
+        })
+        self.dronePosList.append({
+            "type": "marker",
+            "coordinates": [
+                139.588237,
+                35.819651
+
+             ],
+            "properties": {
+                "id": str(time.time())
+            }
+        })
+
 
     def connect(self, droneIP, socketIP, socketPort):
         self.vehicle = connect(droneIP, wait_ready=True)
+        self.arm_and_takeoff(10)
         self.socketIO = SocketIO(socketIP, socketPort, LoggingNamespace) 
         self.socketIO.on("on_aaa_response", self.on_aaa_response)
         self.socketIO.wait()
@@ -51,21 +112,16 @@ class Drone(object):
         return interval_id
 
     def emitPosition(self):
-        x = {
-            "type": "marker",
-            "coordinates": [
-                139.58855534,
-                35.82004085
+        if (self.dronePosLisIdx == 5):
+            self.dronePosLisIdx = 0
 
-             ],
-            "properties": {
-                "id": str(time.time())
-            }
-        }
+        x = self.dronePosList[self.dronePosLisIdx]
 
         # convert into JSON:
         y = json.dumps(x)
         self.socketIO.emit("fromPy", y)
+        # we increment the counter for the index
+        self.dronePosLisIdx += 1 
 
     def on_aaa_response(self, *args):
         if(args[0] == True):
@@ -74,5 +130,39 @@ class Drone(object):
             self.interval = self.set_interval(1, self.emitPosition)
         else :
             print("not connected")
+    
+    def arm_and_takeoff(self, aTargetAltitude):
+        """
+        Arms vehicle and fly to aTargetAltitude.
+        """
+
+        print "Basic pre-arm checks"
+        # Don't try to arm until autopilot is ready
+        while not self.vehicle.is_armable:
+            print " Waiting for vehicle to initialise..."
+            time.sleep(1)
+
+        print "Arming motors"
+        # Copter should arm in GUIDED mode
+        self.vehicle.mode    = VehicleMode("GUIDED")
+        self.vehicle.armed   = True
+
+        # Confirm vehicle armed before attempting to take off
+        while not self.vehicle.armed:
+            print " Waiting for arming..."
+            time.sleep(1)
+
+        print "Taking off!"
+        self.vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+        # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+        #  after Vehicle.simple_takeoff will execute immediately).
+        while True:
+            print " Altitude: ", self.vehicle.location.global_relative_frame.alt
+            #Break and return from function just below target altitude.
+            if self.vehicle.location.global_relative_frame.alt >= aTargetAltitude*0.95:
+                print "Reached target altitude"
+                break
+            time.sleep(1)
 
 
